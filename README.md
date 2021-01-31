0369e466e1b450c4f22555837e1ff1f36ce65e82
Symbol <Symbolistic@live.com>
2021-01-31 00:52:38 -0500

    first commit of working version for vcompression

---
+++ README.md
@@ -0,0 +1,3 @@
+# VCompression
+
+Developed mainly for company use, specifically at my current job. This will automate the task of compressing downloaded images and converting them into WebP format. Combining this with VScraper will save hours of work time and can achieve hours worth of results done with manual work, in seconds using the 2 automated scripts VScraper and VCompression.
---
+++ vcompression.py
@@ -0,0 +1,36 @@
+from PIL import Image
+import PIL
+import os
+import glob
+
+# Start First Compression
+current_working_directory = os.getcwd()
+files = os.listdir()
+
+images = [file for file in files if file.endswith(
+    ('jpg', 'png')) and '' in file]
+
+os.mkdir("Compressed")
+for image in images:
+    image_name = image.split('.')[0]
+
+    img = Image.open(image)
+    img.save("Compressed/" + image, optimized=True, quality=35)
+
+
+# Start WebP Coversion and Compression
+current_working_directory = os.chdir('Compressed')
+files = os.listdir()
+
+images = [file for file in files if file.endswith(
+    ('jpg', 'png')) and '' in file]
+
+os.mkdir("WebP")
+
+for image in images:
+    image_name = image.split('.')[0]
+
+    img = Image.open(image)
+    img.convert('RGB')
+    img.save('WebP/' + image_name + '.webp',
+             'webp', quality=38)
